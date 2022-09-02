3dde89ce322ecea6825a9751204d7ee76329787e
AhmedZein <ahmedzain503@gmail.com>
2022-09-02 15:57:08 +0200

    adding front end configuration

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export POSTGRES_PASSWORD=0598251140
+export POSTGRES_HOST=my-database-deployment-project.ckumvt1tswol.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=deployment
+export AWS_BUCKET=arn:aws:s3:::front-end-deployment-project
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export JWT_SECRET=ahmedzein
+export URL=My-deployment-project-env.eba-5qkdgddq.us-east-1.elasticbeanstalk.com
